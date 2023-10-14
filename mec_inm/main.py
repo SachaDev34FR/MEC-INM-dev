fae5b8130b69bb1e2b855fe28cf818941cd9b315
SachaDEV34FR <sachatcm34690@gmail.com>
2023-10-14 16:59:52 -0400

    add esearch new fonctions to main

--- mec_inm/main.py
+++ mec_inm/main.py
@@ -1,14 +1,16 @@
 from src import pubmed_connect as pconn
 from src import esearch_pubmed as Esearch
+from src import create_reports_from_pubmed as crp
 
 import src.constants as ct
 def run():
     
     pconn.connect_pubmed_database()
-    term="green tea AND neoplasms"
-    liste_pmids = Esearch.esearch_dataset(ct.DB, term)
-    print(liste_pmids)
-    
+    terms="green tea AND neoplasms"
+    liste_pmids = Esearch.esearch_dataset(ct.DB, terms)
+    liste_journaux = Esearch.get_journal_names(terms)
+    resultats_intervalle_dates = Esearch.esearch_dataset_dates(ct.DB, terms, "2020/01/01", "2023/10//01")
+    crp.create_text_reports(liste_pmids, liste_journaux, resultats_intervalle_dates)
     
     
     
