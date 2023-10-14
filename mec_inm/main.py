91eeee2b2f828286df400d30267bf1d079c5c036
SachaDEV34FR <sachatcm34690@gmail.com>
2023-10-14 15:59:35 -0400

    add esearch_pubmed  to main

--- mec_inm/main.py
+++ mec_inm/main.py
@@ -1,8 +1,14 @@
 from src import pubmed_connect as pconn
+from src import esearch_pubmed as Esearch
 
+import src.constants as ct
 def run():
     
     pconn.connect_pubmed_database()
+    term="green tea AND neoplasms"
+    Esearch.esearch_dataset(ct.DB, term)
+    
+    
     
     
 if __name__ == "__main__" :
