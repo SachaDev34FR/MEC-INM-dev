f491e69c8d65dc62018f5fb30ac372f461747dbf
Sacha <sachatorres974@gmail.com>
2023-10-15 15:47:53 -0400

    fix main and functions esearch

--- mec_inm/main.py
+++ mec_inm/main.py
@@ -8,9 +8,12 @@ def run():
     pconn.connect_pubmed_database()
     terms="green tea AND neoplasms"
     liste_pmids = Esearch.esearch_dataset(ct.DB, terms)
-    resultats_intervalle_dates = Esearch.esearch_dataset_dates(terms, "2023/01/01", "2023/10/01")
-    resultats_intervalle_period = Esearch.esearch_datas_period(terms,180)
-    crp.create_text_reports(liste_pmids, resultats_intervalle_dates,resultats_intervalle_period)
+    date_min = '2023/01/01'
+    date_max = '2023/10/01'
+    period = '180'
+    resultats_intervalle_dates = Esearch.esearch_dataset_dates(terms, date_min, date_max)
+    resultats_intervalle_period = Esearch.esearch_datas_period(terms, period)
+    crp.create_text_esearch_reports(liste_pmids, resultats_intervalle_dates,resultats_intervalle_period)
     
     
     
--- mec_inm/src/create_reports_from_pubmed.py
+++ mec_inm/src/create_reports_from_pubmed.py
@@ -5,7 +5,7 @@ import sys
 import src.constants as ct
 
 
-def create_text_reports(a, b, c):
+def create_text_esearch_reports(a, b, c):
 
  with open('report_pubmed_datas.txt', 'w') as file:
   file.write(f"Liste des PMIDs: {str(a)}" + '\n')
